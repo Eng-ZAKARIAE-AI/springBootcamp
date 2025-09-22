c41f9757ff0a23ad77ade5165b8f029d36d9ee64
ZAKARIAE EL HADDOUCHI <200651075+Eng-ZAKARIAE-AI@users.noreply.github.com>
2025-09-22 22:30:57 +0100

    Delete Desktop/learnBYprojects/spring-boot project/patient-app directory
    delete this

--- Desktop/learnBYprojects/spring-boot project/patient-app/src/main/java/net/elhaddochi/patient_app/reporistory/PatientReporistory.java
+++
@@ -1,7 +0,0 @@
-package net.elhaddochi.patient_app.reporistory;
-
-import net.elhaddochi.patient_app.entites.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientReporistory extends JpaRepository<Patient,Long> {
-}
